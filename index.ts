d3eb151b162ee61e4a4c0b5d59fd348cc69e6fe3
Pr1mosz <olechprzemyslaw@gmail.com>
2023-10-27 16:11:32 +0200

    Added error handling and connection to database.

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from 'cors';
 import 'express-async-errors';
+import {handleError, ValidationError} from "./utils/errors";
 
 const app = express();
 
@@ -8,6 +9,12 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }));
 app.use(express.json());
+
+app.get('/', async (req, res) => {
+    throw new ValidationError('Daaamn!')
+})
+
+app.use(handleError);
 
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on http://localhost:3001');
